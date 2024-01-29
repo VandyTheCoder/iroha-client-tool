9682dc929626babff8f844e9cf19c27ba3566028
Vandy Sodanheang <56287711+VandyTheCoder@users.noreply.github.com>
2024-01-29 17:00:35 +0700

    Allow Multiple Hashes

--- src/main/kotlin/IrohaClientTool.kt
+++ src/main/kotlin/IrohaClientTool.kt
@@ -6,17 +6,21 @@ import javax.xml.bind.DatatypeConverter
 
 class IrohaClientTool {
     companion object {
-        private val irohaNodes = listOf("x.x.x.x:50051")
+        private val irohaNodes = listOf(
+            "x.x.x.x:50051"
+        )
         private val irohaPeers = irohaNodes.map {
             val (host, port) = it.split(":")
             IrohaAPI(host, port.toInt())
         }
+        private val trxHashes = listOf(
+            "xxxx",
+            "xxxx"
+        ).map { DatatypeConverter.parseHexBinary(it) }
 
 
         @JvmStatic
         fun main(args: Array<String>) {
-            val trxHash = "xxx"
-
             // Initialize Iroha peers
             println("IROHA_CLIENT :: Initializing Iroha peers ...")
             irohaPeers.forEach { peer ->
@@ -24,6 +28,10 @@ class IrohaClientTool {
                     when (val state = peer.channel.getState(true)) {
                         ConnectivityState.IDLE -> {
                             println("IROHA_CLIENT :: Connection to PEER[${peer.uri}] is idling ...")
+                            Thread.sleep(500) // Wait for 0.5 seconds
+                        }
+                        ConnectivityState.CONNECTING -> {
+                            println("IROHA_CLIENT :: Connecting to PEER[${peer.uri}] ...")
                             Thread.sleep(500) // Wait for 0.5 seconds
                         }
                         ConnectivityState.READY -> {
@@ -37,21 +45,21 @@ class IrohaClientTool {
                 }
             }
             println("IROHA_CLIENT :: Done initializing Iroha peers!")
-
-            // Get transaction status
-            println("IROHA_CLIENT :: Getting transactopm with Hash[$trxHash] status ...")
-            getTransactionStatus(listOf(DatatypeConverter.parseHexBinary(trxHash)))
+            getTransactionStatus()
         }
 
         @OptIn(ExperimentalStdlibApi::class)
-        private fun getTransactionStatus(trxHashes : List<ByteArray>) {
+        private fun getTransactionStatus() {
             for (trxHash in trxHashes) {
+                // Get transaction status
+                println("\nIROHA_CLIENT :: Getting transaction with Hash[${trxHash.toHexString()}] status ...")
                 irohaPeers.forEach {
                     try {
                         val status = it.txStatusSync(trxHash)
                         println("IROHA_CLIENT :: PEER[${it.uri}] - Status of Transaction[${trxHash.toHexString()}] is ${status.txStatus.name}")
                     } catch (e: Exception) { e.printStackTrace() }
                 }
+                println("IROHA_CLIENT :: Done getting transaction with Hash[${trxHash.toHexString()}] status!\n")
             }
         }
     }
